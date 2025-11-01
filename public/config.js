bee23cd78cb0b06387c01288fd6b5021ce54fc18
Mario Reder <mario.reder@pm.me>
2025-11-01 22:19:16 +0100

    Setup DEX with workflow files and configuration

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
-  "VITE_ORDERLY_BROKER_ID": "demo",
+  "VITE_ORDERLY_BROKER_ID": "kimchidex",
   "VITE_ORDERLY_BROKER_NAME": "Kimchi Premium DEX",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
